9523f246161635d914010afa67a80e2945a65c79
micschro <michael.frischeisen-koehler@yworks.com>
2017-04-27 14:09:06 +0200

    Test and fix amd+es6

---
+++ test/amd-es6.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var fs = require('fs');
+var exec = require('child_process').exec;
+var helpers = require('yeoman-test');
+var assert = require('yeoman-assert');
+var opn = require('opn');
+
+var util = require('./support/util');
+var defaultAnswers = require('./support/defaultPromtAnswers');
+
+var answers = Object.assign({},defaultAnswers, {
+  "loadingType": "AMD",
+  "advancedOptions": [
+    "Use yfiles-typeinfo.js",
+    "ECMAScript 6"
+  ]
+});
+
+
+describe('yfiles:amd-es6', function () {
+
+  this.timeout(55000);
+
+  before(function(done) {
+    var that = this;
+    this.app = helpers
+      .run(require.resolve('../generators/app'))
+      .withGenerators([[helpers.createDummyGenerator(),require.resolve('../generators/class')]])
+      .withOptions({
+        'skip-welcome-message': true,
+        'skip-message': true,
+        'skip-install': false
+      })
+      .withPrompts(answers).then(function(dir) {
+        that.dir = dir;
+        done();
+      })
+  });
+
+  describe('check files', function() {
+    it('generates base files', function () {
+      assert.file([
+        'app/index.html',
+        'app/scripts/app.js',
+        'app/styles/yfiles.css',
+        'bower.json'
+      ]);
+      assert.noFile([
+        'app/scripts/license.js',
+        'webpack.config.js',
+        'package.json',
+        'Gruntfile.js'
+      ]);
+    });
+
+  });
+
+  describe('build result', function() {
+
+    it('installed bower files', function() {
+      assert.file([
+        'bower_components/requirejs/require.js'
+      ]);
+    });
+
+    it('runs', function (done) {
+      util.maybeOpenInBrowser(this.dir,done);
+    });
+
+  });
+
+});
